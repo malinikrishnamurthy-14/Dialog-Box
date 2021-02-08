b560c9f0fac9747a753865e3f1690b03c94eb282
Malini Krishnamurthy <72509011+malinikrishnamurthy-14@users.noreply.github.com>
2021-02-08 15:46:02 -0500

    Login modal dialog

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
 function loadDialogBox() {
-  console.log("button clicked");
+  console.log("about button clicked");
   document.getElementById("about-dialog").style.display = "block";
 }
 
 function closeMD() {
   console.log("close button clicked");
   document.getElementById("about-dialog").style.display = "none";
+}
+
+function loadLogin() {
+  console.log("login button clicked");
+  document.getElementById("login-dialog").style.display = "block";
 }
