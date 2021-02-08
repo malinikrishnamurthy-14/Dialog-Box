8a3d80ece68fff869d1d2f091b256c4b6aeedc2a
Malini Krishnamurthy <72509011+malinikrishnamurthy-14@users.noreply.github.com>
2021-02-08 14:16:43 -0500

    first commit

---
+++ index.js
@@ -0,0 +1,9 @@
+function loadDialogBox() {
+  console.log("button clicked");
+  document.getElementById("about-dialog").style.display = "block";
+}
+
+function closeMD() {
+  console.log("close button clicked");
+  document.getElementById("about-dialog").style.display = "none";
+}
